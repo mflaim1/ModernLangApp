8c5d2f8796a95b1edd4c18b8afe149d9e0bc1cef
daviddorsey <Hcky739*>
2016-03-28 13:03:08 -0400

    incorporating firebase

--- natLangMockup/AppDelegate.m
+++ natLangMockup/AppDelegate.m
@@ -7,7 +7,7 @@
 //
 
 #import "AppDelegate.h"
-
+#import <Firebase/Firebase.h>
 @interface AppDelegate ()
 
 @end
@@ -17,6 +17,7 @@
 
 - (BOOL)application:(UIApplication *)application didFinishLaunchingWithOptions:(NSDictionary *)launchOptions {
     // Override point for customization after application launch.
+    self.firebaseRef = [[Firebase alloc] initWithUrl:@"https://modernlang.firebaseio.com/"];
     return YES;
 }
 
